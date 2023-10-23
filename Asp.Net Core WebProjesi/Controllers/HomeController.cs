aa3b531e5486df151aeda433d5c233552098c56e
Okan Tırpancı <okantirpanci96@gmail.com>
2023-10-24 00:32:52 +0300

    Add project files.

---
+++ Asp.Net Core WebProjesi/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Asp.Net_Core_WebProjesi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Asp.Net_Core_WebProjesi.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Asp.Net Core WebProjesi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.Net_Core_WebProjesi.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
