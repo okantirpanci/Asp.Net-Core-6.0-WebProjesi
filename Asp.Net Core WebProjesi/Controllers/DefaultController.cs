fbe970564cd2f7e043209d7774096ca30afa68fd
Okan Tırpancı <okantirpanci96@gmail.com>
2023-10-26 22:43:58 +0300

    add: new controller

---
+++ Asp.Net Core WebProjesi/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_WebProjesi.Controllers
+{
+	public class DefaultController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
