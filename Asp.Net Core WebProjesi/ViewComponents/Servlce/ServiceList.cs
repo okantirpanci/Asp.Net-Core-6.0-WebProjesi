5179f2b9d95c45c4d180810ced1c3f85ff2998e3
Okan Tırpancı <okantirpanci96@gmail.com>
2023-10-27 02:50:44 +0300

    add: viewcomponents

---
+++ Asp.Net Core WebProjesi/ViewComponents/Servlce/ServiceList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_WebProjesi.ViewComponents.Servlce
+{
+    public class ServiceList : ViewComponent
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = serviceManager.TGetList();
+            return View(values);
+        }
+    }
+}
