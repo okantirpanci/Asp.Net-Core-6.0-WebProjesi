e7a4c6bca32d7f9d3b0bcc9b0b65f9ef058c0d6a
Okan Tırpancı <okantirpanci96@gmail.com>
2023-10-30 16:50:41 +0300

    add viewcomponents

---
+++ Asp.Net Core WebProjesi/ViewComponents/Experience/ExperienceList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_WebProjesi.ViewComponents.Experience
+{
+    public class ExperienceList : ViewComponent
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+    }
+}
