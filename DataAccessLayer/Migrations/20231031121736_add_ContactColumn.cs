5a788b02a0656926e741790c6d8cd796b503ab94
Okan Tırpancı <okantirpanci96@gmail.com>
2023-10-31 15:27:50 +0300

    add contactcolumn

---
+++ DataAccessLayer/Migrations/20231031121736_add_ContactColumn.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class add_ContactColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Linkedln",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Linkedln",
+                table: "Contacts");
+        }
+    }
+}
