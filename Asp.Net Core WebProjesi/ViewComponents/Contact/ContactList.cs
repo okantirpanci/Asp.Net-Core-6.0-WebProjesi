dc90aa63aa52de3d13908ac5fc1a87e011ca7940
Okan Tırpancı <okantirpanci96@gmail.com>
2023-10-31 15:27:40 +0300

    add viewcomponents

---
+++ Asp.Net Core WebProjesi/ViewComponents/Contact/ContactList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_WebProjesi.ViewComponents.Contact
+{
+    public class ContactList : ViewComponent
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());   
+        public IViewComponentResult Invoke()
+        {
+            var values = contactManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ Asp.Net Core WebProjesi/ViewComponents/Contact/SendMessage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_WebProjesi.ViewComponents.Contact
+{
+    public class SendMessage : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
