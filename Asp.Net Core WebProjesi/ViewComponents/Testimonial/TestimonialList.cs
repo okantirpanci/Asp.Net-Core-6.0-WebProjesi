5ca05dfd2b0b711e369388edc3a4ba96352d93a6
Okan Tırpancı <okantirpanci96@gmail.com>
2023-10-31 14:48:10 +0300

    add viewcomponents

---
+++ Asp.Net Core WebProjesi/ViewComponents/Testimonial/TestimonialList.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.Net_Core_WebProjesi.ViewComponents.Testimonial
+{
+    public class TestimonialList : ViewComponent
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());   
+        public IViewComponentResult Invoke()
+        {
+            var values = testimonialManager.TGetList();
+
+            return View(values);
+        }
+
+    }
+}
